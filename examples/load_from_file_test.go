e48528998358080290f3b34465dcb27326442ded
alimirjahani <ali.mirjahani@cafebazaar.ir>
2020-08-13 23:04:53 +0430

    add yml file support

--- examples/load_from_file_test.go
+++ examples/load_from_file_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestLoadFromFileOnGlobalConfig(t *testing.T) {
+func TestLoadFromJsonFileOnGlobalConfig(t *testing.T) {
 	g := GlobalConfig{}
 	err := goconfman.LoadFromFile(&g, "global_config.json")
 	if err != nil {
@@ -15,10 +15,10 @@ func TestLoadFromFileOnGlobalConfig(t *testing.T) {
 	}
 
 	expectedG := GlobalConfig{
-		IntegerValue:       1234,
-		FloatValue:         987.3,
-		StringValue:        "globalConfig.StringValue in global_config.json",
-		LocalConfig:        LocalConfig{
+		IntegerValue: 1234,
+		FloatValue:   987.3,
+		StringValue:  "globalConfig.StringValue in global_config.json",
+		LocalConfig: LocalConfig{
 			IntegerValue:      42,
 			FloatValue:        0,
 			StringValue:       "",
@@ -31,7 +31,48 @@ func TestLoadFromFileOnGlobalConfig(t *testing.T) {
 			IntegerValue: 0,
 			FloatValue:   0,
 			StringValue:  "",
-			LocalConfig:  LocalConfig{
+			LocalConfig: LocalConfig{
+				IntegerValue:      71,
+				FloatValue:        0,
+				StringValue:       "",
+				SliceValue:        nil,
+				SliceOfSliceValue: nil,
+				MapValue:          nil,
+				ComplicatedValue:  nil,
+			},
+		},
+	}
+
+	if reflect.DeepEqual(g, expectedG) == false {
+		t.Errorf("g is different from the expectedG. here's the diff: \n%s", cmp.Diff(g, expectedG))
+	}
+}
+
+func TestLoadFromYmlFileOnGlobalConfig(t *testing.T) {
+	g := GlobalConfig{}
+	err := goconfman.LoadFromFile(&g, "global_config.yml")
+	if err != nil {
+		t.Fatalf("Error in LoadFromMap: %s", err.Error())
+	}
+
+	expectedG := GlobalConfig{
+		IntegerValue: 1234,
+		FloatValue:   987.3,
+		StringValue:  "globalConfig.StringValue in global_config.json",
+		LocalConfig: LocalConfig{
+			IntegerValue:      42,
+			FloatValue:        0,
+			StringValue:       "",
+			SliceValue:        nil,
+			SliceOfSliceValue: nil,
+			MapValue:          nil,
+			ComplicatedValue:  nil,
+		},
+		NonGoConfManConfig: NonGoConfManConfig{
+			IntegerValue: 0,
+			FloatValue:   0,
+			StringValue:  "",
+			LocalConfig: LocalConfig{
 				IntegerValue:      71,
 				FloatValue:        0,
 				StringValue:       "",
@@ -59,7 +100,34 @@ func TestLoadFromFileOnNonGoconfmanConfig(t *testing.T) {
 		IntegerValue: 0,
 		FloatValue:   0,
 		StringValue:  "",
-		LocalConfig:  LocalConfig{
+		LocalConfig: LocalConfig{
+			IntegerValue:      71,
+			FloatValue:        13.31,
+			StringValue:       "",
+			SliceValue:        nil,
+			SliceOfSliceValue: nil,
+			MapValue:          nil,
+			ComplicatedValue:  nil,
+		},
+	}
+
+	if reflect.DeepEqual(ng, expectedNG) == false {
+		t.Errorf("ng is different from the expectedNG. here's the diff: \n%s", cmp.Diff(ng, expectedNG))
+	}
+}
+
+func TestLoadFromYmlFileOnNonGoconfmanConfig(t *testing.T) {
+	ng := NonGoConfManConfig{}
+	err := goconfman.LoadFromFile(&ng, "non_goconfman_config.json")
+	if err != nil {
+		t.Fatalf("Error in LoadFromMap: %s", err.Error())
+	}
+
+	expectedNG := NonGoConfManConfig{
+		IntegerValue: 0,
+		FloatValue:   0,
+		StringValue:  "",
+		LocalConfig: LocalConfig{
 			IntegerValue:      71,
 			FloatValue:        13.31,
 			StringValue:       "",
@@ -84,10 +152,10 @@ func TestLoadFromFileAfterLoadFromDefaultsOnGlobalConfig(t *testing.T) {
 	}
 
 	expectedG := GlobalConfig{
-		IntegerValue:       1234,
-		FloatValue:         987.3,
-		StringValue:        "globalConfig.StringValue in global_config.json",
-		LocalConfig:        LocalConfig{
+		IntegerValue: 1234,
+		FloatValue:   987.3,
+		StringValue:  "globalConfig.StringValue in global_config.json",
+		LocalConfig: LocalConfig{
 			IntegerValue:      42,
 			FloatValue:        31.4,
 			StringValue:       "in local config",
@@ -100,7 +168,7 @@ func TestLoadFromFileAfterLoadFromDefaultsOnGlobalConfig(t *testing.T) {
 			IntegerValue: 0,
 			FloatValue:   0,
 			StringValue:  "",
-			LocalConfig:  LocalConfig{
+			LocalConfig: LocalConfig{
 				IntegerValue:      71,
 				FloatValue:        31.4,
 				StringValue:       "in local config",
--- pkg/goconfman/file.go
+++ pkg/goconfman/file.go
@@ -2,26 +2,38 @@ package goconfman
 
 import (
 	"encoding/json"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 func LoadFromFile(config interface{}, filePath string) error {
+
+	fileExt := filepath.Ext(filePath)
 	configFile, err := os.Open(filePath)
 	if err != nil {
 		return err
 	}
-	defer configFile.Close()
+	if fileExt == ".json" {
+		defer configFile.Close()
+		configBytes, err := ioutil.ReadAll(configFile)
+		if err != nil {
+			return err
+		}
 
-	configBytes, err := ioutil.ReadAll(configFile)
-	if err != nil {
-		return err
+		err = json.Unmarshal(configBytes, config)
+		if err != nil {
+			return err
+		}
+	} else if fileExt == ".yml" || fileExt == ".yaml" {
+		yamlFile, err := ioutil.ReadAll(configFile)
+		if err != nil {
+			return err
+		}
+		if err = yaml.Unmarshal(yamlFile, config); err != nil {
+			return err
+		}
 	}
-
-	err = json.Unmarshal(configBytes, config)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
