ff977c15eff68a7369130fa9179011e76cf7e430
Sayed Soroush Hashemi <hashemi.soroush@gmail.com>
2021-06-30 16:40:38 +0430

    Revert LoadFromDefaults

--- pkg/goconfman/defaults.go
+++ pkg/goconfman/defaults.go
@@ -8,10 +8,10 @@ type ConfigWithDefaults interface {
 
 // TODO: this method should be able to access unexported fields
 func LoadFromDefaults(config interface{}) {
+	internal.LoadRecursive(config, LoadFromDefaults)
+	
 	c, ok := config.(ConfigWithDefaults)
 	if ok {
 		c.BindDefaults()
 	}
-
-	internal.LoadRecursive(config, LoadFromDefaults)
 }
